e3d4d847803335e689bb27e6a13781a34063e5e0
Pranjul Singh <singhpranjul001@gmail.com>
2024-05-10 19:42:39 +0530

    Added custom access denied page.

--- src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -38,7 +38,7 @@ public class DemoSecurityConfig {
         http.authorizeHttpRequests(configurer->configurer.requestMatchers("/").hasRole("EMPLOYEE").requestMatchers("/leaders/**").hasRole("MANAGER").requestMatchers("/systems/**").hasRole("ADMIN").anyRequest().authenticated())
                 .formLogin(form->form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
                 .logout(logout->logout.permitAll())
-                        ;
+                .exceptionHandling(configurer->configurer.accessDeniedPage("/access-denied"));
         return http.build();
     }
 }
