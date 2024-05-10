dd0aa5ba778549150953adb917684614d8d6eafd
Pranjul Singh <singhpranjul001@gmail.com>
2024-05-10 19:33:43 +0530

    Restricted the access based on roles.

--- src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -35,7 +35,7 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer->configurer.anyRequest().authenticated())
+        http.authorizeHttpRequests(configurer->configurer.requestMatchers("/").hasRole("EMPLOYEE").requestMatchers("/leaders/**").hasRole("MANAGER").requestMatchers("/systems/**").hasRole("ADMIN").anyRequest().authenticated())
                 .formLogin(form->form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
                 .logout(logout->logout.permitAll())
                         ;
