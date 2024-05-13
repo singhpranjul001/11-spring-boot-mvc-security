bc57b6cf64de42d37264e593c785767f25d6cb9a
Pranjul Singh <singhpranjul001@gmail.com>
2024-05-13 18:47:55 +0530

    Added JDBC authentication support for Bcrypt and Plain text.

--- src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/practice/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,14 +3,32 @@ package com.practice.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // Add support for JDBC... no more hard coded users...
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer->configurer.requestMatchers("/").hasRole("EMPLOYEE").requestMatchers("/leaders/**").hasRole("MANAGER").requestMatchers("/systems/**").hasRole("ADMIN").anyRequest().authenticated())
+                .formLogin(form->form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
+                .logout(logout->logout.permitAll())
+                .exceptionHandling(configurer->configurer.accessDeniedPage("/access-denied"));
+        return http.build();
+    }
+
+    /* Hard Coded user, password and roles
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -32,13 +50,5 @@ public class DemoSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer->configurer.requestMatchers("/").hasRole("EMPLOYEE").requestMatchers("/leaders/**").hasRole("MANAGER").requestMatchers("/systems/**").hasRole("ADMIN").anyRequest().authenticated())
-                .formLogin(form->form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
-                .logout(logout->logout.permitAll())
-                .exceptionHandling(configurer->configurer.accessDeniedPage("/access-denied"));
-        return http.build();
-    }
+    */
 }
